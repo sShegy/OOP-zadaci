04602731e0856e3c9d86c955ae9460417449bb02
Shegy <milansegan55@gmail.com>
2024-07-09 15:07:25 +0200

    .

--- RAF_IT_DAYS_2/src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module module_name {
-    requires java.base;
-    requires javafx.controls;
-    requires javafx.graphics;
-    exports mvc;
-}
--- RAF_IT_DAYS_2/src/main/java/mvc/view/MainStage.java
+++
@@ -1,9 +0,0 @@
-package mvc.view;
-
-public class MainStage {
-
-    private
-
-
-
-}
