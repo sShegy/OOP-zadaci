92abc709ea5b0f5981cf203744102e42692d09f6
Shegy <msegan13323ri@raf.rs>
2024-07-22 17:37:34 +0200

    .

--- Aplikacija/src/main/java/mvc/controller/BtnIgraj.java
+++ Aplikacija/src/main/java/mvc/controller/BtnIgraj.java
@@ -19,11 +19,11 @@ public class BtnIgraj implements EventHandler<ActionEvent> {
         String r = rockPaperScissors.getCbIzaberi(player).getSelectionModel().getSelectedItem();
 
         if (r.equals("Rock")) {
-            rockPaperScissors.setImage(player, new Image("file:/E:/OOP-zadaci/Aplikacija/images/Screenshot_3.png"));
+            rockPaperScissors.setImage(player, new Image("file:/E:/OOP-zadaci/Aplikacija/images/Rock.png"));
         } else if (r.equals("Paper")) {
-            rockPaperScissors.setImage(player, new Image("file:/E:/OOP-zadaci/Aplikacija/images/Screenshot_1.png"));
+            rockPaperScissors.setImage(player, new Image("file:/E:/OOP-zadaci/Aplikacija/images/Paper.png"));
         } else {
-            rockPaperScissors.setImage(player, new Image("file:/E:/OOP-zadaci/Aplikacija/images/Screenshot_2.png"));
+            rockPaperScissors.setImage(player, new Image("file:/E:/OOP-zadaci/Aplikacija/images/Scissors.png"));
         }
     }
 }
--- Aplikacija/src/main/java/mvc/view/RockPaperScissors.java
+++ Aplikacija/src/main/java/mvc/view/RockPaperScissors.java
@@ -9,16 +9,17 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import mvc.controller.BtnBet;
 import mvc.controller.BtnIgraj;
 
 public class RockPaperScissors extends Stage {
     private static RockPaperScissors instance;
 
+    private Button btnBet = new Button("Bet");
 
     private Button btnIgraj1 = new Button("Igraj");
     private ImageView imageView1 = new ImageView();
     private ComboBox<String> cbIzaberi1 = new ComboBox<>();
-
 
     private Button btnIgraj2 = new Button("Igraj");
     private ImageView imageView2 = new ImageView();
@@ -32,44 +33,26 @@ public class RockPaperScissors extends Stage {
 
     private void ucitajSliku1() {
         try {
-            String imagePath = "file:/E:/OOP-zadaci/Aplikacija/images/Screenshot_1.png";
-            System.out.println("Loading image for player 1 from: " + imagePath);
+            String imagePath = "file:/E:/OOP-zadaci/Aplikacija/images/Rock.png";
             Image image1 = new Image(imagePath);
             imageView1.setImage(image1);
-
             imageView1.setFitWidth(100);
             imageView1.setFitHeight(100);
             imageView1.setPreserveRatio(true);
-
-            if (image1.isError()) {
-                System.err.println("Error loading image for player 1: " + image1.getException().getMessage());
-            } else {
-                System.out.println("Image for player 1 loaded successfully from path: " + imagePath);
-            }
         } catch (Exception e) {
-            System.err.println("Exception loading image for player 1: " + e.getMessage());
             e.printStackTrace();
         }
     }
 
     private void ucitajSliku2() {
         try {
-            String imagePath = "file:/E:/OOP-zadaci/Aplikacija/images/Screenshot_2.png";
-            System.out.println("Loading image for player 2 from: " + imagePath);
+            String imagePath = "file:/E:/OOP-zadaci/Aplikacija/images/Paper.png";
             Image image2 = new Image(imagePath);
             imageView2.setImage(image2);
-
             imageView2.setFitWidth(100);
             imageView2.setFitHeight(100);
             imageView2.setPreserveRatio(true);
-
-            if (image2.isError()) {
-                System.err.println("Error loading image for player 2: " + image2.getException().getMessage());
-            } else {
-                System.out.println("Image for player 2 loaded successfully from path: " + imagePath);
-            }
         } catch (Exception e) {
-            System.err.println("Exception loading image for player 2: " + e.getMessage());
             e.printStackTrace();
         }
     }
@@ -82,6 +65,7 @@ public class RockPaperScissors extends Stage {
     }
 
     private void init() {
+        btnBet.setOnAction(new BtnBet(this));
 
         VBox vb1 = new VBox();
         vb1.setAlignment(Pos.CENTER);
@@ -94,23 +78,21 @@ public class RockPaperScissors extends Stage {
 
         vb1.getChildren().addAll(cbIzaberi1, imageView1, btnIgraj1);
 
-
         VBox vb2 = new VBox();
         vb2.setAlignment(Pos.CENTER);
         vb2.setSpacing(5);
 
         cbIzaberi2.getItems().addAll("Rock", "Paper", "Scissors");
-        cbIzaberi2.getSelectionModel().selectFirst();
+        cbIzaberi2.getSelectionModel().select(1);
 
         btnIgraj2.setOnAction(new BtnIgraj(this, 2));
 
         vb2.getChildren().addAll(cbIzaberi2, imageView2, btnIgraj2);
 
-
         HBox hbox = new HBox();
         hbox.setAlignment(Pos.CENTER);
-        hbox.setSpacing(20);
-        hbox.getChildren().addAll(vb1, vb2);
+        hbox.setSpacing(100);
+        hbox.getChildren().addAll(vb1, vb2, btnBet);
 
         Scene scene = new Scene(hbox, 1000, 600);
         this.setScene(scene);
@@ -120,11 +102,27 @@ public class RockPaperScissors extends Stage {
         return player == 1 ? cbIzaberi1 : cbIzaberi2;
     }
 
+    public ImageView getImageView1() {
+        return imageView1;
+    }
+
+    public ImageView getImageView2() {
+        return imageView2;
+    }
+
     public void setImage(int player, Image image) {
         if (player == 1) {
             imageView1.setImage(image);
         } else {
             imageView2.setImage(image);
         }
+    }
+
+    public ComboBox<String> getCbIzaberi1() {
+        return cbIzaberi1;
+    }
+
+    public ComboBox<String> getCbIzaberi2() {
+        return cbIzaberi2;
     }
 }
